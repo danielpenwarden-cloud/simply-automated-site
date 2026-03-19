8286433df0b1f1079e7b0902337e3f2e90fee127
danielpenwarden-cloud <danielpenwarden@gmail.com>
2026-03-19 10:05:31 +0700

    Added Meta

--- simply-automated-site/src/app/layout.js
+++ simply-automated-site/src/app/layout.js
@@ -30,6 +30,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="facebook-domain-verification" content="ulvi43ze7wtdadsosa5pmkzz7jy9wr" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,400;0,9..144,500;1,9..144,300;1,9..144,400;1,9..144,500&display=swap" rel="stylesheet" />
